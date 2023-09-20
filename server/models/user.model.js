fa694d2bfbc46b4e089b63cfac6c8d86c7d6b4e4
James Watson <jameywatson134@gmail.com>
2023-09-20 11:37:37 -0700

    updated models and applied default font

---
+++ server/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    profilePic: {
+        type: String,
+        required: true,
+        default: "https://static.thenounproject.com/png/5163895-200.png"
+    }
+
+
+},
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User
