648f0c7dba9331e7f0b19007841024e2c629fcc7
James Watson <jameywatson134@gmail.com>
2023-10-12 15:57:18 -0700

    Chat Box frame is fixed

--- client/src/Components/ChatBox.jsx
+++ client/src/Components/ChatBox.jsx
@@ -9,17 +9,18 @@ const ChatBox = (fetchAgain, setFetchAgain) => {
 
   return (
     <Box
-      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
-      alignItems={"center"}
-      flexDir={"column"}
-      background={"white"}
-      padding={3}
-      width={{ base: "100%", medium: "68%"}}
-      borderRadius={"large"}
-      borderWidth={"1px"}>
-      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>
+      d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
     </Box>
-  )
+  );
 }
 
 export default ChatBox
--- client/src/Views/ChatPage.jsx
+++ client/src/Views/ChatPage.jsx
@@ -18,7 +18,7 @@ const ChatPage = () => {
     <div style={{ width: "100%" }}>
         {user && <SideDrawer />}
         <Box display="flex" justifyContent="space-between" width="100%" height="91.5vh" padding="10px">
-            {user && ( <MyChats fetchAgain={fetchAgain} />)}
+            {user && ( <MyChats fetchAgain={fetchAgain}/>)}
             {user && ( <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />)}
         </Box>
     </div>
