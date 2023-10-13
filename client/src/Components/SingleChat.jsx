ec9e5d5d1dbe0d4ec29a8affe453b058bb165f38
James Watson <jameywatson134@gmail.com>
2023-10-13 00:13:45 -0700

    update

--- client/src/Components/SingleChat.jsx
+++ client/src/Components/SingleChat.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import { ChatState } from '../Context/ChatProvider';
+import { Box, IconButton, Text } from '@chakra-ui/react';
+import {  ArrowBackIcon, ArrowLeftIcon } from '@chakra-ui/icons';
 
 const SingleChat = (fetchAgain, setFetchAgain) => {
 
 const {user, selectedChat, setSelectedChat} = ChatState();
 
     return <>{
+        selectedChat ? (
+            <>
+            <Text
+            fontSize={{ base: "30px", md: "32px"}}
+            pb={3}
+            px={2}
+            width={"100%"}
+            display={"flex"}
+            justifyContent={{ base: "space-between"}}
+            alignItems={"center"}
+            fontFamily={"futura"}
+            >
+            <IconButton
+            display={{ base: "flex", medium: "none"}}
+            icon={<ArrowBackIcon/>}
+            onClick={() => setSelectedChat("")}
+            />
+            </Text>
+            </>
 
+        ) : (
+            <Box display = "flex" alignItems = "center" justifyContent = "center" height ="100%">
+                <Text fontSize = "25px" fontFamily = "futura" pb={3}>
+            Select an user to start chat.
+                </Text>
+
+            </Box>
+        )
     
     } </>;
     
