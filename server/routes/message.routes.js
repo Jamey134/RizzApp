f734393adb589bd3d4a51a6701a496c84e158a34
James Watson <jameywatson134@gmail.com>
2023-10-17 01:35:36 -0700

    allMessage function is working

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -4,7 +4,7 @@ const { protect } = require('../middleware/authMiddleware');
 module.exports = app => {
     app.post("/api/message", protect, MessageController.sendMessage)
 
-    //app.get("/api/message/:chatId", protect, allMessages)
+    app.get("/api/message/:chatId", protect, MessageController.allMessages)
 
 
 
