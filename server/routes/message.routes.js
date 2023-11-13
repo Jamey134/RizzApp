d324fd5ffb0c6b4c9ae922d9f0c1f6f7089e29c8
James Watson <jameywatson134@gmail.com>
2023-11-13 00:38:12 -0800

    branch update

--- server/routes/message.routes.js
+++
@@ -1,9 +0,0 @@
-const MessageController = require("../controllers/message.controller");
-const { protect } = require('../middleware/authMiddleware');
-
-module.exports = app => {
-    app.post("/api/message", protect, MessageController.sendMessage)
-
-    app.get("/api/message/:chatId", protect, MessageController.allMessages)
-
-};
